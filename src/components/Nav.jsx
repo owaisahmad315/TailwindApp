81056be10cfe54435d73a6c9543eba22b85d964e
Owais Ahmad <owaisahmad014@gmail.com>
2023-09-07 21:14:53 +0500

    added navlinks

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,5 +1,6 @@
 import { headerLogo} from "../assets/images";
 import { hamburger } from "../assets/icons";
+import { navLinks } from "../constants";
 
 const Nav = () => {
   return (
@@ -16,7 +17,16 @@ const Nav = () => {
         </a>
 
         <ul className="flex-1 flex justify-center gap-16 max-lg:hidden">
-
+          {navLinks.map((item) => (
+            <li key={item.label}>
+              <a
+              href={item.href}
+              className="font-montserrat leading-normal text-lg text-slate-gray"
+              >
+                {item.label}
+              </a>
+            </li>
+          ))}
         </ul>
       </nav>
     </header>
--- src/constants/index.js
+++ src/constants/index.js
@@ -121,4 +121,4 @@ export const socialMedia = [
     { src: facebook, alt: "facebook logo" },
     { src: twitter, alt: "twitter logo" },
     { src: instagram, alt: "instagram logo" },
-];
+]; 

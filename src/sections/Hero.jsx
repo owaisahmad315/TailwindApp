50aa5849de84e7f18a316344e0fb7193b7f3b055
Owais Ahmad <owaisahmad014@gmail.com>
2023-09-09 21:46:03 +0500

    ..

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -4,6 +4,12 @@ const Hero = () => {
     <section id="home" className="w-full flex xl:flex-row flex-col justify-center min-h-screen gap-10 max-container">
       <div className="w-full xl:w-2/5 flex flex-col justify-center items-start max-xl:padding-x pt-28">
         <p>Our Summer Collection</p>
+        <h1>
+          <span>The New Arrival</span>
+          <br />
+          <span>Nike</span>
+          Shoes
+        </h1>
         
       </div>
     </section>

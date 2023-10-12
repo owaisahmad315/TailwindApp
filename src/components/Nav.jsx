62600628d6ada1e1bf974e34ff1269acc3d16208
Owais Ahmad <owaisahmad014@gmail.com>
2023-10-13 01:28:58 +0500

    hero is complete

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -29,7 +29,7 @@ const Nav = () => {
           ))}
         </ul>
 
-        <div className="hidden max-lg:blo   ck">
+        <div className="hidden max-lg:block">
           <img 
           src={hamburger} 
           alt="humberger"

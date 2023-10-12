799f0bdd8db8cee730abe2a23ade77026ef6c4bc
Owais Ahmad <owaisahmad014@gmail.com>
2023-10-13 00:22:48 +0500

    hero image

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,9 +1,9 @@
 import Button from "../components/Button";
 import {arrowRight} from "../assets/icons";
-import { statistics } from "../constants";
+import {shoes, statistics } from "../constants";
 import { bigShoe1 } from "../assets/images";
 import ShoeCard from "../components/ShoeCard";
-
+ 
  
 const Hero = () => {
   return (
@@ -51,7 +51,7 @@ const Hero = () => {
           className="object-contain relative z-10"
         />
 
-        <div>
+        <div className="flex sm:gap-6 gap-4 absolute -bottom-[5%] sm:left-[10%] max-sm:px-6">
           {shoes.map((shoe) =>(
             <div key={shoe}>
               <ShoeCard 
